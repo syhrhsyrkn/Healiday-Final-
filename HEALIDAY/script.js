69385d2815bc1aee57e0cabc0420ea50e8b82cd3
syhrhsyrkn <117880899+syhrhsyrkn@users.noreply.github.com>
2023-01-20 23:24:06 +0800

    Add files via upload

---
+++ HEALIDAY/script.js
@@ -0,0 +1,51 @@
+function GuideForm1(){
+  document.getElementById('formSpencer').textContent = "Here is Spencer contact details: 0123456789 (Spencer)";
+}
+
+function GuideForm2(){
+  document.getElementById('formAmelia').textContent = "Here is Amelia contact details: 0112223456 (Amelia)";
+}
+
+function GuideForm3(){
+  document.getElementById('formMostafa').textContent = "Here is Mostafa contact details: 0123334567 (Mostafa)";
+}
+
+function GuideForm4(){
+  document.getElementById('formLesti').textContent = "Here is Lesti contact details: 0156785532 (Lesti)";
+}
+
+function GuideForm5(){
+  document.getElementById('formKindel').textContent = "Here is Kindel contact details: 0186583454 (Kindel)";
+}
+
+function GuideForm6(){
+  document.getElementById('formAndy').textContent = "Here is Andy contact details: 0193574332 (Andy)";
+}
+
+function Location1(){
+  window.location.href = "mabul_location.html";
+  }
+
+  function Location2(){
+    window.location.href = "tasikbiru_location.html";
+  }
+
+  function Location3(){
+    window.location.href = "mossyForest_location.html";
+  }
+
+  function Location4(){
+    window.location.href = "skyMirror_location.html";
+  }
+
+  function ButtonBack(){
+    window.location.href = "suburb.html";
+  }
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
